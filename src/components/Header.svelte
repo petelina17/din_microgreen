0762195625e8529e361290a92c7f2444a8ded426
petelina17 <petelina17@gmail.com>
2020-12-31 19:35:37 +0100

    Header menu + init icons, make them fixed, transparent background

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -12,9 +12,43 @@
 
 </script>
 
-
-<div class="h-24 bg-gray-200">
-  {#if user != null}
-    <div>User name: {user.firstName}, surname: {user.secondName}</div>
-  {/if}
+<div class="bg-gray-600 bg-opacity-75 fixed w-full">
+  <div class="header flex items-center justify-end wrapper">
+    <nav class="flex header4 text-white uppercase opacity-100">
+      <div>Live kamera</div>
+      <div>Butik</div>
+      <div>Recept</div>
+      <div>Artiklar</div>
+      <div>Forum</div>
+    </nav>
+  </div>
+  <div class="absolute social-icons">
+    <div class="w-12 h-12 mb-3 rounded-full bg-gray-200">fb</div>
+    <div class="w-12 h-12 mb-3 rounded-full bg-gray-200">twitter</div>
+    <div class="w-12 h-12 rounded-full bg-gray-200">insta</div>
+  </div>
+  <div class="absolute right-icons">
+    <div class="w-12 h-12 mb-3 rounded-full bg-gray-200">konto</div>
+    <div class="w-12 h-12 mb-3 rounded-full bg-gray-200">basket</div>
+  </div>
 </div>
+
+<style>
+  .header {
+    height: 3.5rem;
+  }
+
+  nav > div {
+    margin-left: 1em;
+  }
+
+  .social-icons {
+    left: 2rem;
+    top: 15rem;
+  }
+
+  .right-icons {
+    right: 2rem;
+    top: 15rem;
+  }
+</style>
