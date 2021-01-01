2c3c5025ab395ed054503915461d19d55ed270e9
petelina17 <petelina17@gmail.com>
2021-01-01 16:55:21 +0100

    Top Banner component for homepage

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -2,11 +2,18 @@
   import Cart from './Cart.svelte'
   import SlideContainer from './SlideContainer.svelte'
   import Header from './Header.svelte'
+  import TopBanner from './TopBanner.svelte'
+
 
   let showCart = false
 
 </script>
+
 <Header/>
+
+<TopBanner/>
+
+<!--
 <div style="height: 600px; background-color: #728e30">Home</div>
 
 <div style="height: 2000px; background-color: white">
@@ -21,3 +28,4 @@
   {/if}
 
 </div>
+-->
---
+++ src/components/TopBanner.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Logo from './Logo.svelte'
+
+</script>
+
+<div class="banner">
+  <div class="wrapper pt-20">
+    <Logo/>
+  </div>
+</div>
+
+<style>
+  .banner {
+    height: 600px;
+    background-color: #728e30;
+  }
+</style>
