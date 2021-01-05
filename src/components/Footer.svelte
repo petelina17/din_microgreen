6fdbe878f7a17e7ce5bf02a2ce5d68efa5bd0395
petelina17 <petelina17@gmail.com>
2021-01-05 16:53:11 +0100

    Footer menu-items hover effect

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 
-<div class="w-full footer">
+<div class="w-full footer select-none">
   <div class="wrapper h-full flex flex-row relative">
 
     <div class="absolute left-0 bottom-0 pb-8 text-gray-500">&copy Natalia Petelina. Utbildningsproject 2020-2021</div>
@@ -24,10 +24,19 @@
     </div>
   </div>
 </div>
+
 <style>
   .footer {
     background-color: #393939;
     height: 17.5rem;
+  }
+  .menu-item {
+    cursor: pointer;
+  }
+
+  .menu-item:hover {
+    transform: scale(1.03);
+    color: white;
   }
 
 </style>
