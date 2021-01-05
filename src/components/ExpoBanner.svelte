0a932b115a6bfbc081b714a9e27e81cc549d1732
petelina17 <petelina17@gmail.com>
2021-01-05 18:10:41 +0100

    Refactor ArticleBanner component and remake it to reusable ExpoBanner for Articles and Forum

---
+++ src/components/ExpoBanner.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Button from 'smelte/src/components/Button'
+
+  export let img = 'img'
+  export let title = 'title'
+  export let text = 'text'
+  export let buttonPrimary = null
+  export let buttonSecondary = null
+  export let orient = 'left'
+
+</script>
+
+<div class="banner select-none bg-offwhite">
+  <div class="wrapper h-full flex items-center {orient === 'left' ? 'flex-row' : 'flex-row-reverse'}">
+
+    <div class="w-2/3 text-left">
+      <div class="header1 mb-6">
+        {title}
+      </div>
+
+      <div class="text1">
+        {text}
+      </div>
+
+      <div class="header4 mt-12">
+        <!-- show button if only prop for button is defined-->
+        {#if buttonPrimary != null}
+          <Button remove="text-sm uppercase" add="rounded-full w-64 h-16 header4 mr-8 mb-4">
+            {buttonPrimary.label}
+          </Button>
+        {/if}
+
+        {#if buttonSecondary != null}
+          <Button light color="colorC"
+                  remove="text-sm text-white uppercase"
+                  add="rounded-full w-64 h-16 header4 text-offblack mb-4">
+            {buttonSecondary.label}
+          </Button>
+        {/if}
+
+      </div>
+    </div>
+
+    <div class="w-1/3 text-center">
+      <img class="m-auto" src="./img/{img}" alt="bild" height="292" width="320">
+    </div>
+
+  </div>
+</div>
+
+<style>
+  .banner {
+    height: 43.75rem;
+    /*background-color: #f2f2f2;*/
+    /*border-bottom: 1px solid #a0aec0;*/
+  }
+</style>
--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -3,7 +3,7 @@
   import SlideContainer from './SlideContainer.svelte'
   import Header from './Header.svelte'
   import TopBanner from './TopBanner.svelte'
-  import ArticleBanner from './ArticleBanner.svelte'
+  import ExpoBanner from './ExpoBanner.svelte'
   import LinkBanner from './LinkBanner.svelte'
   import ProductCard from './ProductCard.svelte'
   import ProductBanner from './ProductBanner.svelte'
@@ -12,13 +12,37 @@
 
   let showCart = false
 
+  let article = {
+    img: 'redis.jpg',
+    title: 'Lorem ipsum',
+    text: `Donec volutpat enim nibh, sit amet eleifend velit congue ac. Suspendisse maximus
+           felis sed nulla cursus facilisis. Vivamus sollicitudin augue vel nisi rutrum,
+           non varius ipsum molestie. Proin auctor est sit amet justo laoreet,
+           ultricies faucibus libero tincidunt. Praesent elementum tellus sit amet ligula
+           fermentum, eu tempus purus commodo. Nunc nibh sapien.`,
+    buttonPrimary: {label: 'Läs mer', link: ''},
+    buttonSecondary: {label: 'Till artiklar', link: ''}
+  }
+
+  let forum = {
+    img: 'forum5.jpg',
+    title: 'Doremipsum',
+    text: `Volutpat enim nibh, sit amet eleifend velit congue ac. Suspendisse maximus
+           felis sed nulla cursus facilisis. Vivamus sollicitudin augue vel nisi rutrum,
+           non varius ipsum molestie. Proin auctor est sit amet justo laoreet,
+           ultricies faucibus libero tincidunt. Praesent elementum tellus sit amet ligula
+           fermentum, eu tempus purus commodo. Nunc nibh sapien.`,
+    buttonPrimary: {label: 'Till forum', link: ''}
+  }
+
 </script>
 
 <Header/>
 
 <TopBanner/>
 
-<ArticleBanner/>
+<ExpoBanner img={article.img} title={article.title} text={article.text}
+            buttonPrimary={article.buttonPrimary} buttonSecondary={article.buttonSecondary} />
 
 <LinkBanner title="Övervaka dina växter live" button="Visa mer"
             img="fullmoon3-b.jpg" link="/live-cam"/>
@@ -28,8 +52,10 @@
 <LinkBanner title="" button="Receptbok"
             img="smoky.jpg" link="/recipes"/>
 
+<ExpoBanner img={forum.img} title={forum.title} text={forum.text}
+            buttonPrimary={forum.buttonPrimary} orient="right" />
 
-<Footer />
+<Footer/>
 
 
 <!--
