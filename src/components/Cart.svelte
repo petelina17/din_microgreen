da4305614bc0bb5e40bd81aab8cf0f3ad3c70df7
petelina17 <petelina17@gmail.com>
2021-01-10 20:01:52 +0100

    WIP: cart component + cart item

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -1,15 +1,60 @@
 <script>
+  import SimpleHeader from './SimpleHeader.svelte'
+  import {userStore} from '../store'
+  import CartItem from './CartItem.svelte'
+  import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+  import Button  from 'smelte/src/components/Button'
 
+  $: cartTotal = $userStore.cartList.reduce((sum,item)=>{
+    return sum + item.productData.price * item.qty
+  }, 0)
+
+  function nextHandler() {
+
+  }
 </script>
 
 <div class="w-full h-full main">
-  <div class="uppercase text-3xl">cart</div>
+  <SimpleHeader noClose={true} title="Din varukorg" icon={faShoppingCart}
+    bgColor="bg-gray-700"
+  />
+
+  <div class="flex flex-wrap pt-12">
+
+    <!--
+      Tailwind CSS Responsive
+      https://v1.tailwindcss.com/docs/width#responsive
+    -->
+    <div class="lg:w-2/3 w-full px-5">
+      {#each $userStore.cartList as item}
+        <CartItem title={item.productData.title}
+                  subtitle={item.productData.subtitle}
+                  qty={item.qty}
+                  unitPrice={item.productData.price}
+                  productId={item.productData.productId}
+                  img={item.productData.img}
+        />
+      {/each}
+    </div>
+    <div class="text-center lg:w-1/3 w-full">
+
+      <div class="header2 mb-8">{cartTotal.toFixed(2)} kr</div>
+
+      <Button remove="text-sm uppercase" add="rounded-full w-64 h-16 header4 mb-4"
+              on:click={nextHandler}>
+        Till kassan
+      </Button>
+    </div>
+
+  </div>
+
+
 </div>
 
 <style>
   .main {
     margin: auto;
-    background-color: #1d8ef1;
+    /*background-color: #eafafc;*/
   }
 
 </style>
---
+++ src/components/CartItem.svelte
@@ -0,0 +1,58 @@
+<script>
+  import {faTrashAlt} from '@fortawesome/free-regular-svg-icons'
+  import Icon from 'fa-svelte'
+
+  export let title = 'no passaran'
+  export let subtitle = 'subheader'
+  export let qty = 12
+  export let unitPrice = 22.05
+  export let productId = '1'
+  export let img = 'camera2.jpg'
+
+  // dynamic var
+  $: totalPrice = qty * unitPrice
+
+  function deleteHandler() {
+
+  }
+
+</script>
+
+<div class="flex py-6 border-b body-1 border-gray-400">
+  <div class="w-24 text-center pl-3">
+    <div class="w-16 h-16 rounded-full bg-red-500 bg-cover img-placeholder"
+         style={"background-image: url('img/" + img + "');"}>&nbsp</div>
+  </div>
+
+  <div class="text-left px-3 w-1/2">
+    <div class="header4">{title}</div>
+    <div class="text1">{subtitle}</div>
+  </div>
+
+  <div class="px-3 w-1/6">
+    <div class="header4">{unitPrice.toFixed(2)} <span class="text-base">kr</span></div>
+    <div class="header4">{qty} <span class="text-base">st.</span></div>
+  </div>
+
+  <div class="px-3 w-1/6">
+    <div class="header4">{totalPrice.toFixed(2)} <span class="text-base">kr</span></div>
+  </div>
+
+  <div>
+    <div class="cursor-pointer w-10 h-10 rounded-full hover:bg-gray-200
+                flex items-center justify-center"
+    on:click={deleteHandler}>
+      <Icon icon={faTrashAlt}/>
+    </div>
+  </div>
+
+
+</div>
+
+<style>
+  .img-placeholder {
+    background-position-x: center;
+    background-size: 115%;
+    /*background-position-y: 2rem;*/
+  }
+</style>
