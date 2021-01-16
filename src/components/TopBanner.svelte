bd7196ea6c2c43c2a5e0ec9a945217caf977e2f8
petelina17 <petelina17@gmail.com>
2021-01-16 22:35:15 +0100

    refactor topBanner responsivity

--- src/components/TopBanner.svelte
+++ src/components/TopBanner.svelte
@@ -6,9 +6,9 @@
 <div class="banner">
   <div class="wrapper pt-20 text-offwhite">
     <Logo/>
-    <div class="text-header3 mt-3 px-16 sm:text-header1 sm:mt-20">#Superfood på endast 6-10 dagar</div>
+    <div class="text-header3 mt-10 sm:text-header1 sm:mt-20">#Superfood på endast 6-10 dagar</div>
 
-    <div class="text-lg px-16 mt-4 sm:mt-0 sm:text-header3">Odla själv hemma VS beställ hos oss och se det växa LIVE</div>
+    <div class="px-4 text-lg mt-4 sm:mt-3 sm:text-header3 mb:mt-8">Odla själv hemma VS beställ hos oss och se det växa LIVE</div>
 
     <div class="flex flex-wrap mt-8 sm:justify-between pb-5">
       <div class="w-full sm:w-auto mb-4">
