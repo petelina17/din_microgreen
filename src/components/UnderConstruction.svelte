7c9083754920f51c8bc72a83ee8438d90307fb9b
petelina17 <petelina17@gmail.com>
2021-01-17 22:09:48 +0100

    UnderConstruction component responsivity

--- src/components/UnderConstruction.svelte
+++ src/components/UnderConstruction.svelte
@@ -8,14 +8,15 @@
 
 <div class="mx-auto" style="max-width: 60rem;">
   <div class="flex flex-row flex-wrap mt-32 mb-12 text-center bg-primary-300">
-    <div class="w-1/3 bg-cover bg-center"
+    <div class="w-1/3 bg-center
+                sm:w-1/3 sm:bg-cover sm:bg-center"
          style="background-image: url('/img/forum4.jpg')">
       &nbsp
     </div>
-    <div class="w-2/3 py-32 text-center text-header3 text-white">Kommer snart!</div>
+    <div class="w-2/3 py-32 text-center text-header4 sm:text-xl sm:text-header3 text-white">Kommer snart!</div>
   </div>
 
-  <div class="text-center hover:underline text-blue-500 cursor-pointer text-xl"
+  <div class="text-center text-xl text-blue-500 underline sm:hover:underline  cursor-pointer "
        on:click={goBackHandler}>
     Gå tillbaka
   </div>
